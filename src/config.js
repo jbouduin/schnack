2fc69777ec2eeb4b6f44b074bd65601e5f241e68
Johan Bouduin <johan@der-e-coach.de>
2019-11-09 23:30:35 +0100

    removed re-implemented stuff from original sources

--- src/config.js
+++ src/config.js
@@ -6,26 +6,7 @@ const webpush = require('web-push');
 const vapidKeys = webpush.generateVAPIDKeys();
 
 nconf
-    .argv()
-    .file({ file: './config.json' })
-    .env()
     .defaults({
-        admins: [1],
-        schnack_host: `http://localhost`,
-        database: {
-            comments: 'comments.db',
-            sessions: 'sessions.db'
-        },
-        port: 3000,
-        template: {
-            login_status:
-                '(signed in as %USER% :: <a class="schnack-signout" href="#">sign out</a>)'
-        },
-        date_format: 'MMMM DD, YYYY - h:mm a',
-        notification_interval: 300000,
-        oauth: {
-            secret: crypto.randomBytes(64).toString('hex')
-        },
         notify: {
             webpush: {
                 vapid_public_key: vapidKeys.publicKey,
--- src/helper.js
+++ src/helper.js
@@ -3,7 +3,6 @@ const { URL } = require('url');
 const queries = require('./db/queries');
 const config = require('./config');
 
-const schnack_domain = getSchnackDomain();
 function send_file(file, admin_only) {
     return send_string(fs.readFileSync(file, 'utf-8'), admin_only);
 }
@@ -32,17 +31,6 @@ function error(err, request, reply, code) {
     return false;
 }
 
-function getUser(request) {
-    if (config.get('dev'))
-        return { id: 1, name: 'Dev', display_name: 'Dev', admin: true, trusted: 1 };
-    const { user } = request.session.passport || {};
-    return user;
-}
-
-function isAdmin(user) {
-    return user && user.id && config.get('admins').indexOf(user.id) > -1;
-}
-
 function checkOrigin(origin, callback) {
     // origin is allowed
     if (
@@ -54,49 +42,3 @@ function checkOrigin(origin, callback) {
 
     callback(new Error('Not allowed by CORS'));
 }
-
-function checkValidComment(db, slug, user_id, comment, replyTo, callback) {
-    if (comment.trim() === '') return callback(new Error("the comment can't be empty"));
-    // check duplicate comment
-    db.get(queries.get_last_comment, [slug], (err, row) => {
-        if (err) return callback(err);
-        if (row && row.comment.trim() === comment && row.user_id === user_id) {
-            return callback(new Error('the exact comment has been entered before'));
-        }
-        // @todo: check for cyclic replies
-        callback(null);
-    });
-}
-
-function getSchnackDomain() {
-    const schnack_host = config.get('schnack_host');
-    try {
-        const schnack_url = new URL(schnack_host);
-
-        if (schnack_url.hostname === 'localhost') {
-            return schnack_url.hostname;
-        } else {
-            const schnack_domain = schnack_url.hostname
-                .split('.')
-                .slice(1)
-                .join('.');
-            return schnack_domain;
-        }
-    } catch (error) {
-        console.error(
-            `The schnack_host value "${schnack_host}" doesn't appear to be a proper URL. Did you forget "http://"?`
-        );
-        process.exit(-1);
-    }
-}
-
-module.exports = {
-    send_file,
-    send_string,
-    error,
-    getUser,
-    isAdmin,
-    checkOrigin,
-    checkValidComment,
-    getSchnackDomain
-};
